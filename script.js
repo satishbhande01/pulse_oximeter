d4728fc1775f52345efb7f9051e5c4dfc1e6a69b
Satish Bhande <162822122+satishbhande01@users.noreply.github.com>
2024-10-24 20:23:08 +0530

    Update script.js
    Updated the entire script.js because the modal for share wasn't working.

--- script.js
+++ script.js
@@ -57,3 +57,31 @@ backToTopButton.addEventListener('click', function () {
         });
     }, 500); // Slight delay to allow the page to scroll to the top
 });
+
+// Modal related functionality
+
+// Get the modal
+var modal = document.getElementById("shareModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("shareButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block"; // Show modal
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none"; // Hide modal
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none"; // Hide modal
+    }
+}
